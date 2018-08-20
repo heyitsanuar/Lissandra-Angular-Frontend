af92232c540d2f1b3bad9e1fed9fe81235a89aba
Anuar Jimenez <heyitsanuar@gmail.com>
2018-08-20 00:52:33 -0500

    Added product model.

---
+++ src/app/models/product.ts
@@ -0,0 +1,12 @@
+export class Product{
+    constructor(
+        public _id: string,
+        public name: string,
+        public description: string,
+        public cost: string,
+        public category: string,
+        public type: string,
+        public images: [string],
+        public sale: string
+    ){}
+}
